2ff8410f1717c14370dfd285ca0e49a89b0cb8ee
Jøran Vagnby Lillesand <lillesand@gmail.com>
2015-05-10 11:49:36 +0200

    Support for loading a repo and scanning a single file. Will probably behave weirdly for multiple files.

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 'use strict';
 var through = require('through2'),
     gutil = require('gulp-util'),
-    PluginError = gutil.PluginError;
+    path = require('path'),
+    os = require('os'),
+    PluginError = gutil.PluginError,
+    repo    = require('retire/lib/repo'),
+    scanner = require('retire/lib/scanner');
 
 module.exports = function() {
-    var retire = function (file, encoding, callback) {
+
+    var gulpRetire = function (file, encoding, callback) {
+        var options = {
+            jsRepository: 'https://raw.github.com/RetireJS/retire.js/master/repository/jsrepository.json',
+            nodeRepository: 'https://raw.github.com/RetireJS/retire.js/master/repository/npmrepository.json'
+        };
+
         if (file.isStream()) {
             return callback(createError(file, 'Streams are not supported'));
         }
+
+        scanner.on('vulnerable-dependency-found', function(e) {
+            return callback(createError(file, 'Vulnerable library found'));
+        });
+
+        scanner.on('dependency-found', function(e) {
+
+        });
+
+        var vulnerabilityRepoLoaded = function(repository) {
+            scanner.scanJsFile(file.path, repository, options);
+        };
+
+        if (!options.nocache) {
+            options.cachedir = path.resolve(os.tmpdir(), '.retire-cache/');
+        }
+        if (/^http[s]?:/.test(options.jsRepository)) {
+            repo.loadrepository(options.jsRepository, options).on('done', vulnerabilityRepoLoaded);
+        } else {
+            repo.loadrepositoryFromFile(options.jsRepository, options).on('done', vulnerabilityRepoLoaded);
+        }
     };
-    return through.obj(retire);
+    return through.obj(gulpRetire);
 };
 
 
--- test/test.js
+++ test/test.js
@@ -27,6 +27,28 @@ test('should fail on streams', function(t) {
     stream.end();
 });
 
+test('should give error on known bad libraries', function(t) {
+    t.plan(2);
+
+    var streamFile = new Vinyl({
+        path: "./test/files/jquery-1.6.js"
+    });
+
+    var stream = gulpRetire();
+
+    stream.on('data', function() {
+        t.fail("we shouldn't have gotten here");
+    });
+
+    stream.on('error', function(e) {
+        t.ok(e instanceof Error, 'argument should be of type error');
+        t.equal(e.plugin, 'gulp-retire', 'error is from gulp-retire');
+    });
+
+    stream.write(streamFile);
+    stream.end();
+});
+
 
 function stringStream() {
     var stream = new Readable();
